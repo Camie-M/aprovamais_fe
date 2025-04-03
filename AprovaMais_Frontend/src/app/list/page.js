ed18f5f1392938456d06b22951cce877993ea00e
breno.ferrari <breno.ferrari@fiap.com.br>
2025-04-02 23:39:09 -0300

    simulando consumo api + v1 gerar pdf

--- AprovaMais_Frontend/src/app/list/page.js
+++ AprovaMais_Frontend/src/app/list/page.js
@@ -1,30 +1,88 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 import BoxQuestoes from "../components/boxQuestoes";
 import Questoes from "../components/questoes";
-import listaQuestoes from "../../mocks/mocksQuestões.json";
 import Button from "../components/button";
+import html2pdf from "html2pdf.js";
 export default function List() {
-  const handleClick = () => {
-    alert("Botão clicado!");
+  const [dadosGerais, setDadosGerais] = useState({
+    prova: "",
+    anosSelecionados: "",
+    materia: "",
+    topicos: [],
+  });
+  const [listaQuestoes, setListaQuestoes] = useState([]);
+  const pdfRef = useRef(null);
+
+  useEffect(() => {
+    const fetchMockData = async () => {
+      try {
+        const response = await fetch("/mocks/mocksQuestões.json");
+        const data = await response.json();
+
+        setDadosGerais({
+          prova: data.prova || "Prova Desconhecida",
+          anosSelecionados: data.anos || "Ano não informado",
+          materia: data.materia || "Matéria não informada",
+          topicos: data.questoes.flatMap((q) => q.topicos || []),
+        });
+
+        setListaQuestoes(data.questoes || []);
+      } catch (error) {
+        console.error("Erro ao carregar os dados:", error);
+      }
+    };
+    fetchMockData();
+  }, []);
+
+  const gerarPDF = () => {
+    const input = pdfRef.current;
+    if (!input) return;
+
+    html2pdf()
+      .set({
+        margin: 10,
+        filename: `Questoes_${dadosGerais.prova}.pdf`,
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2, useCORS: true },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      })
+      .from(input)
+      .save();
   };
 
   return (
-    <div className={styles.container}>
-      <h1>Lista de questões</h1>
+    <>
+      <Button texto={"Gerar PDF"} onClick={gerarPDF} />
+      <div ref={pdfRef} className={styles.container}>
+        <BoxQuestoes>
+          <p>
+            Lista de questões da <b>{dadosGerais.prova}</b> dos anos{" "}
+            <b>{dadosGerais.anosSelecionados}</b> da matéria{" "}
+            <b>{dadosGerais.materia}</b> sobre os tópicos{" "}
+            <b>{dadosGerais.topicos.join(", ")}</b>.
+          </p>
+        </BoxQuestoes>
 
-      {listaQuestoes.map((item, i) => (
-        <div className={styles.container__questoes} key={i}>
-          <BoxQuestoes>
-            <Questoes questao={item} />
-          </BoxQuestoes>
-          <div className={styles.container__questoes__buttons}>
-            <Button texto={"Mostrar Gabarito"} onClick={handleClick} />
-            <Button texto={"Mostrar Solução"} onClick={handleClick} />
-            {/* <input></ <Button texto={"Mostrar Gabarito"} />input> */}
+        {listaQuestoes.map((item, i) => (
+          <div className={styles.container__questoes} key={i}>
+            <BoxQuestoes>
+              <Questoes questao={item} />
+            </BoxQuestoes>
+            <div className={styles.container__questoes__buttons}>
+              <Button
+                texto={"Mostrar Gabarito"}
+                onClick={() => alert(item.resposta)}
+              />
+              <Button
+                texto={"Mostrar Solução"}
+                onClick={() => alert(item.solucao)}
+              />
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
