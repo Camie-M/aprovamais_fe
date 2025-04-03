6d3b20f9a5bd41152aa9d31d4d02bf5ca2a10d36
Valdir Sampaio <134952759+valdirsampaio@users.noreply.github.com>
2025-04-03 19:48:12 -0300

    Teste CI/CD

--- AprovaMais_Backend/src/index.ts
+++ AprovaMais_Backend/src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  res.send('API de Questões está no ar!');
+  res.send('API de Questões está on!');
 });
 
 app.listen(port, () => {
