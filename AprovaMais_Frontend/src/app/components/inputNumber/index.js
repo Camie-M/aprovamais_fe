7ea8ef38dd298373cb3b699faf817d64a359383c
Camila Moura <camilamoura@MacBook-Air-de-Camila.local>
2025-04-07 20:12:53 -0300

    added filters

--- AprovaMais_Frontend/src/app/components/inputNumber/index.js
+++ AprovaMais_Frontend/src/app/components/inputNumber/index.js
@@ -1,12 +1,23 @@
 import styles from "./styles.module.scss";
 
-export default function InputNumber({ placeholder, label }) {
+export default function InputNumber({ placeholder, label, value, onChange }) {
+  const handleChange = (e) => {
+    const numericValue = e.target.value.replace(/\D/g, ""); // remove caracteres não numéricos
+    onChange?.(numericValue); // chama onChange se existir
+  };
+
   return (
     <div className={styles.inputNumber}>
       <label htmlFor={label} className={styles.srOnly}>
         {label}
       </label>
-      <input placeholder={placeholder} id={label} />
+      <input
+        type="text"
+        id={label}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 }
