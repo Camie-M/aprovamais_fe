48dfc20951f6b05889be8bd1110c4c0b8a411a70
breno.ferrari <breno.ferrari@fiap.com.br>
2025-04-13 16:26:33 -0300

    Refatoracao Menu + ajuste tela de login + tela de questoes

--- AprovaMais_Frontend/src/app/page.js
+++ AprovaMais_Frontend/src/app/page.js
@@ -3,11 +3,21 @@ import styles from "./page.module.scss";
 import Filters from "@/app/components/filters";
 import Hero from "@/app/components/hero";
 import Menu from "@/app/components/menu";
-
+import Link from "next/link";
 export default function Home() {
+  const routes = [
+    { path: "/login", label: "Login" },
+    { path: "/dashboard", label: "Dashboard" },
+  ];
   return (
     <main className={styles.wrapper}>
-      <Menu />
+      <Menu>
+        {routes.map((route, index) => (
+          <Link key={index} href={route.path}>
+            {route.label}
+          </Link>
+        ))}
+      </Menu>
       <Hero />
       <Filters />
     </main>
